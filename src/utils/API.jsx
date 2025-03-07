9d1ea6d069d8ac633c176a82a3e22c54d7b7644f
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-03-07 10:51:17 +0500

    Api

--- src/utils/API.jsx
+++ src/utils/API.jsx
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://dummyjson.com/auth";
+
+console.log("API BASE URL:", BASE_URL); 
+
 export const API = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: BASE_URL,
 });
 
 export default API;
